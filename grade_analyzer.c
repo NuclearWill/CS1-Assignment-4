fb65f705aa83fa61581e09a9280796141da6e7a5
Will Thomsen <64432254+NuclearWill@users.noreply.github.com>
2025-04-19 00:01:58 +0000

    one working file

--- grade_analyzer.c
+++ grade_analyzer.c
@@ -3,9 +3,48 @@
 #include <string.h>
 #include <math.h>
 
-// custom header file
-#include "grade_analyzer.h"
+#define MAX_NAME_LENGTH 30
 
+// linked list Student structure
+typedef struct Student {
+    char first[30], last[30];
+    int score;
+    struct Student* next;
+} Student;
+    
+
+// function to open the file
+FILE* openFile();
+// function to close the file
+void closeFile(FILE* file);
+
+// function to create a new Student
+Student* createStudent(char* firstName, char* lastName, int score);
+
+// function to process the file and create a linked list
+Student* processFile(Student* head);
+
+// function to print the linked list
+void printList(Student* head);
+
+// function to sort the linked list using merge sort method
+Student* mergeSort(Student* head);
+// function to get the middle Student of the linked list
+Student* getMiddle(Student* head);
+// function to merge two sorted linked lists
+Student* sortedMerge(Student* left, Student* right);
+
+// function to print the highest score in a sorted list
+void printHighestScore(Student* head);
+// function to print the lowest score in a sorted list
+int printLowestScore(Student* head);
+// function to print the median score in a sorted list
+void printMedianScore(Student* head, int count);
+// function to print the average score in a sorted list
+void printAverageScore(Student* head, int count);
+
+// function to print the top 5 scores in a sorted list
+void printTopFive(Student* head);
 
 int main(int argc, char *argv[]) {
 
